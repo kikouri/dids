8e7c13eb7652e757cbf7b284f911bfb57dc7c479
vivekradya <vivekradya@8cfcad29-148c-6f87-c5cc-fe02b709c840>
2010-12-09 20:01:16 +0000

    Com interface, falta organizar código e ver excepções.

---
+++ trunk/Tracker/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CommModule.Messages;
+using System.Collections;
+
+namespace Tracker
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1(ThreadWorker tw)
+        {
+            InitializeComponent(tw);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArrayList activeNodes = tw.hashTableToArray();
+            if (activeNodes == null)
+            {
+                return;
+            }
+            listBox1.Items.Clear();
+            foreach (Node n in activeNodes)
+            {
+                listBox1.Items.Add("Last seen " + n.IPAddress + ":" + n.port + " at " + n.LastTime);
+            }
+        }
+    }
+}
